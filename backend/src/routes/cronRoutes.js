5bd66400c7eec3f3ec7aa51c7bb0225efb92cf22
Dhanushcj <dhanushchakravarthy18@gmail.com>
2026-07-13 15:53:28 +0530

    fix: allow GET requests for cron job

--- backend/src/routes/cronRoutes.js
+++ backend/src/routes/cronRoutes.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const { triggerPromos } = require('../controllers/cronController');
 
 router.post('/trigger-promos', triggerPromos);
+router.get('/trigger-promos', triggerPromos);
 
 module.exports = router;
